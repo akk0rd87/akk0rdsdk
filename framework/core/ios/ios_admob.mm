1cd15035de066bc3716bbbc9d46e48daf22c2ee2
Akk0rd87ReadOnly <mytestmail@yandex.ru>
2018-02-02 08:51:54 +0300

    +

--- framework/core/ios/ios_admob.mm
+++ framework/core/ios/ios_admob.mm
@@ -2,10 +2,10 @@
 #include "ios_admob.h"
 //@import GoogleMobileAds;
 
-#import <GoogleMobileAds/GoogleMobileAds.h>
+//#import <GoogleMobileAds/GoogleMobileAds.h>
 
 bool AdMobiOS::Init(const char* PublisherID, int Formats)
 {
-    [GADMobileAds configureWithApplicationID:@"YOUR_ADMOB_APP_ID"];
+    //[GADMobileAds configureWithApplicationID:@"YOUR_ADMOB_APP_ID"];
     return true;
 }
