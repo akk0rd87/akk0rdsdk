558728390ae5dda82a0f3b88ce99b12005cbf8c7
akk0rd87 <amkad87@gmail.com>
2018-02-03 14:31:16 +0300

    +

--- clear.bat
+++
@@ -1,7 +0,0 @@
-call rmdir .git /s
-call git init
-call git add .
-call git commit -m "Initial commit"
-
-call git remote add origin https://akk0rd87@bitbucket.org/akk0rd87/akk0rdsdk.git
-call git push -u --force origin master
