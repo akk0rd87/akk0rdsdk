b8f1e41a98bc35d8f4d3765f1b9af9f1575d06ab
akk0rd87 <amkad87@gmail.com>
2018-02-04 13:23:49 +0300

    +

--- framework/basewrapper.cpp
+++ framework/basewrapper.cpp
@@ -493,7 +493,8 @@ AkkordPoint BWrapper::GetScreenSize()
 {
     AkkordPoint WSize;
     //SDL_GetWindowSize(CurrentContext.CurrentWindow, &WSize.x, &WSize.y);
-	SDL_GL_GetDrawableSize(CurrentContext.CurrentWindow, &WSize.x, &WSize.y);
+	//SDL_GL_GetDrawableSize(CurrentContext.CurrentWindow, &WSize.x, &WSize.y);
+	SDL_GetRendererOutputSize(CurrentContext.CurrentRenderer, &WSize.x, &WSize.y);
     return WSize;
 };
 
