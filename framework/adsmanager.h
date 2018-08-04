0246fce064ad1310be6e3ba0e5985d2b3b7998d3
Amir <amkad87@gmail.com>
2018-08-04 23:48:48 +0300

    +=+

--- framework/adsmanager.h
+++ framework/adsmanager.h
@@ -16,10 +16,11 @@ class adsManager
 	// список AdUnit-ов
 	static std::vector<AdMobUnit> AdMobUnits;
 	
-	const Uint32 ShowDelay     = 3 * 60; // 3 минуты  секундах
-	const Uint32 LoadDelay     = 3;      // 3 секунды
-	Uint32       LastShowed    = 0;
-	Uint32       AdMobLastLoad = 0;
+	Uint32                      ShowDelay        = 3 * 60; // 3 минуты  секундах
+	Uint32                      LoadDelay        = 3;      // 3 секунды
+	Uint32                      LastShowed       = 0;
+	Uint32                      AdMobLastLoad    = 0;
+	decltype(AdMobUnits.size()) currentAdmobUnit = std::numeric_limits<decltype(AdMobUnits.size())>::max();
 
 	// время в секундах с момента старта
 	Uint32 GetSeconds()
@@ -33,13 +34,56 @@ class adsManager
 		else                 return LastShowed + ShowDelay;
 	}
 	
+	// выбираем подходящий по времени Unit
 	void ChooseAdmobAdBlock()
 	{
+		auto CurrentTm = GetSeconds();
+		auto NextTm    = GetInterstitialNextShowTime();
 
+		decltype(AdMobUnits.size()) searchUnit = std::numeric_limits<decltype(AdMobUnits.size())>::max();
+
+		// Если новое время еще наступило
+		if (NextTm > CurrentTm)
+		{
+			auto RemainingTime = NextTm - CurrentTm;
+
+			// ищем подходящий, время должно быть отсортировано по возрастанию
+			for (decltype(AdMobUnits.size())i = 0; i < AdMobUnits.size(); ++i)
+				if (RemainingTime <= AdMobUnits[i].TimePriority)
+				{
+					searchUnit = i;
+					break;
+				}
+
+			// Если не нашли, берем последний
+			if (searchUnit == std::numeric_limits<decltype(AdMobUnits.size())>::max())
+				searchUnit = AdMobUnits.size() - 1;
+		}
+		else // если просрочили
+		{
+			// берем первый, с самым низким приоритетом
+			searchUnit = 0;
+		}
+
+		if (currentAdmobUnit != searchUnit)
+		{
+			currentAdmobUnit = searchUnit;
+			AdMob::InterstitialSetUnitId(AdMobUnits[0].Id.c_str());
+		}
 	};
 
 public:
 	
+	void SetIntersitialShowDelay(Uint32 DelaySeconds)
+	{
+		this->ShowDelay = DelaySeconds;
+	};
+
+	void SetIntersitialLoadDelay(Uint32 DelaySeconds)
+	{
+		this->LoadDelay = DelaySeconds;
+	};
+
 	void AddAdMobUnit(const std::string& Id, Uint32 TimePriority)
 	{		
 		AdMobUnits.emplace_back(AdMobUnit(Id, TimePriority));
