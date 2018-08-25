f265d87a58e21a47dd14b41f53adebf774b7ca39
Amir <amkad87@gmail.com>
2018-08-25 13:14:52 +0300

    ++

--- libraries/SDL_image/android/ndk_rebuild.bat
+++ libraries/SDL_image/android/ndk_rebuild.bat
@@ -1,5 +1,5 @@
-rem ndk to 10c
 call cls
+set APP_STL := c++_shared
 set APP_PLATFORM=android-14
 set NDK_PROJECT_PATH=%AKKORD_SDK_HOME%\libraries\SDL_image\SDL_image
 set APP_BUILD_SCRIPT=%NDK_PROJECT_PATH%\Android.mk
