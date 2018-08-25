ed774a0c1f1ba96e40fbba391c7186cd23fe7846
Amir <amkad87@gmail.com>
2018-08-25 12:06:27 +0300

    +

--- libraries/SDL/android/ndk_rebuild.bat
+++ libraries/SDL/android/ndk_rebuild.bat
@@ -1,9 +1,8 @@
-rem ndk to 10c
 call cls
+set APP_STL := c++_shared
 set APP_PLATFORM=android-14
 set NDK_PROJECT_PATH=%AKKORD_SDK_HOME%\libraries\SDL\SDL
 set APP_BUILD_SCRIPT=%NDK_PROJECT_PATH%\Android.mk
-rem set LOCAL_C_INCLUDES=C:\CommonVersioning\Akk0rdSDK\libraries\SDL\SDL\include
 
 call %ANDROID_NDK_HOME%/ndk-build -e clean
 call %ANDROID_NDK_HOME%/ndk-build -e
