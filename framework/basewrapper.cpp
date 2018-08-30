497d4270fb0ef589a5e9ceddb14a68cb2b2f4448
Amir <amkad87@gmail.com>
2018-08-30 18:01:13 +0300

    Пока убрал этот вызов. Так как не понятно, есть ли от него какая-то польза.

--- framework/basewrapper.cpp
+++ framework/basewrapper.cpp
@@ -19,8 +19,8 @@ static LogParamsStruct LogParams;
 bool BWrapper::Init(Uint32 flags)
 {
     //Если раскомментить нижеприведенную строку, то на винде валится при закрытии окна приложения (не консоли)
-    if (SDL_SetMemoryFunctions(std::malloc, std::calloc, std::realloc, std::free) != 0)
-        logError("SDL_SetMemoryFunctions error %s", SDL_GetError());
+    //if (SDL_SetMemoryFunctions(std::malloc, std::calloc, std::realloc, std::free) != 0)
+      //  logError("SDL_SetMemoryFunctions error %s", SDL_GetError());
  
 
     if (SDL_Init(flags) != 0)
