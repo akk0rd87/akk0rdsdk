d2251bc558000b8d8c88a08cc8f79af6759da2a9
Amir Kadyrov [macos] <amkad87@yandex.ru>
2019-06-22 17:32:30 +0300

    update ios SDL and SDL_image build system from command line

--- libraries/SDL/ios/lipo_create.sh
+++ libraries/SDL/ios/lipo_create.sh
@@ -1,4 +1,4 @@
 rm libSDL2.a
 xcodebuild -project ../SDL/Xcode-iOS/SDL/SDL.xcodeproj -target libSDL-iOS -configuration Release -sdk iphoneos
 xcodebuild -project ../SDL/Xcode-iOS/SDL/SDL.xcodeproj -target libSDL-iOS -configuration Release -sdk iphonesimulator
-lipo -create ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2_image.a ../SDL/Xcode-iOS/SDL/build/Release-iphonesimulator/libSDL2_image.a -output libSDL2.a
+lipo -create ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2.a ../SDL/Xcode-iOS/SDL/build/Release-iphonesimulator/libSDL2.a -output libSDL2.a
