5c97b0be2e25ca48b0de845a9493ecd89fc179cd
Amir <amkad87@gmail.com>
2019-06-22 15:30:13 +0300

    Update ndk_rebuild.bat

--- libraries/SDL_image/android/ndk_rebuild.bat
+++ libraries/SDL_image/android/ndk_rebuild.bat
@@ -1,8 +1,10 @@
 call cls
 set APP_STL := c++_shared
-set APP_PLATFORM=android-14
+set APP_PLATFORM=android-16
 set NDK_PROJECT_PATH=%AKKORD_SDK_HOME%\libraries\SDL_image\SDL_image
 set APP_BUILD_SCRIPT=%NDK_PROJECT_PATH%\Android.mk
+set NDK_LIBS_OUT=%AKKORD_SDK_HOME%\libraries\SDL_image\android\lib
 
+rd /s/q lib
 call %ANDROID_NDK_HOME%/ndk-build -e clean
 call %ANDROID_NDK_HOME%/ndk-build -e
