927d307350779f1a903a0218a767f669fdf45c31
Amir <amkad87@gmail.com>
2019-06-22 17:26:04 +0300

    Update lipo_create.sh

--- libraries/SDL/ios/lipo_create.sh
+++ libraries/SDL/ios/lipo_create.sh
@@ -1,1 +1,4 @@
-lipo -create iosDevice/libSDL2.a simulator/libSDL2.a -output libSDL2.a
+rm libSDL2.a
+xcodebuild -project ../SDL/Xcode-iOS/SDL/SDL.xcodeproj -target libSDL-iOS -configuration Release -sdk iphoneos
+xcodebuild -project ../SDL/Xcode-iOS/SDL/SDL.xcodeproj -target libSDL-iOS -configuration Release -sdk iphonesimulator
+lipo -create ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2_image.a ../SDL/Xcode-iOS/SDL/build/Release-iphonesimulator/libSDL2_image.a -output libSDL2.a
