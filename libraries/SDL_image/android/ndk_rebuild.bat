3a2e6765479af347ac37b10c9240ed201ad83321
Amir <amkad87@gmail.com>
2019-06-22 16:08:29 +0300

    Update ndk_rebuild.bat

--- libraries/SDL_image/android/ndk_rebuild.bat
+++ libraries/SDL_image/android/ndk_rebuild.bat
@@ -5,6 +5,8 @@ set NDK_PROJECT_PATH=%AKKORD_SDK_HOME%\libraries\SDL_image\SDL_image
 set APP_BUILD_SCRIPT=%NDK_PROJECT_PATH%\Android.mk
 set NDK_LIBS_OUT=%AKKORD_SDK_HOME%\libraries\SDL_image\android\lib
 
+set SUPPORT_WEBP=FALSE
+
 rd /s/q lib
 call %ANDROID_NDK_HOME%/ndk-build -e clean
 call %ANDROID_NDK_HOME%/ndk-build -e
