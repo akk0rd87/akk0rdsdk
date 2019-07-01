0f79b19529b6ed17234cc3261550316e7312aa99
Amir <amkad87@gmail.com>
2019-07-01 13:10:54 +0300

    Update basewrapper.h

--- framework/basewrapper.h
+++ framework/basewrapper.h
@@ -242,6 +242,8 @@ public:
 
 	//Запрещаем создавать экземпляр класса BWrapper
 	BWrapper() = delete;
+	BWrapper(BWrapper&& rhs) = delete; // Перемещающий: конструктор
+	BWrapper& operator= (BWrapper&& rhs) = delete; // Оператор перемещающего присваивания
 };
 
 class AkkordTexture
