e4eb61f800921e29633c55ce2097eea4cfcc24c5
Amir <amkad87@gmail.com>
2019-07-02 12:55:52 +0300

    Update deploy.bat

--- libraries/SDL/win32/deploy.bat
+++ libraries/SDL/win32/deploy.bat
@@ -1,4 +1,5 @@
 del /q Release\*
-copy ..\SDL\VisualC\Win32\Release\SDL2.dll Release\
-copy ..\SDL\VisualC\Win32\Release\SDL2.lib Release\
-copy ..\SDL\VisualC\Win32\Release\SDL2main.lib Release\
+set source_path=..\SDL\VisualC\Win32\Release
+copy %source_path%\SDL2.dll Release\
+copy %source_path%\SDL2.lib Release\
+copy %source_path%\SDL2main.lib Release\
