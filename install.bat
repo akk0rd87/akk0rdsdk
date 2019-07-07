002a9df5a165a14ffc8a2e395adad66cfa62ae01
Amir <amkad87@gmail.com>
2019-07-07 19:26:22 +0300

    add install bat

--- install.bat
+++ install.bat
@@ -1,1 +1,4 @@
 setx AKKORD_SDK_HOME %~dp0
+git config core.autocrlf false
+git config core.whitespace -cr-at-eol,trailing-space,space-before-tab,tab-in-indent
+copy /Y tools\githooks\pre-commit .git\hooks
