2994d3a8f37645a846d6c37d05b9f06301b54809
Amir <amkad87@gmail.com>
2019-07-07 17:42:34 +0300

    Чистка

--- tools/xpath/xpath.bat
+++ tools/xpath/xpath.bat
@@ -1,45 +1,45 @@
 @if (@X)==(@Y) @end /* JScript comment
     @echo off
-	
-	set "file=%~f1"
-	set "xpath=%~2"
-	set "option=%~3"
-	
-	if "%~2" equ "" (
-		goto :printHelp
-	)
-	
-	for %%# in ("-h" "-help" "/h" "/help" "") do (
-		if /i "%~1" equ %%# (
-			goto :printHelp
-		)
-	)
-	
-	if not exist "%file%" (
-		echo file "%~1" does not exist
-		exit /b 4
-	) 
-	
-	
+
+    set "file=%~f1"
+    set "xpath=%~2"
+    set "option=%~3"
+
+    if "%~2" equ "" (
+        goto :printHelp
+    )
+
+    for %%# in ("-h" "-help" "/h" "/help" "") do (
+        if /i "%~1" equ %%# (
+            goto :printHelp
+        )
+    )
+
+    if not exist "%file%" (
+        echo file "%~1" does not exist
+        exit /b 4
+    )
+
+
     cscript //E:JScript //nologo "%~f0" /file:"%file%" /xpath:"%xpath%" /option:"%option%"
 
     exit /b %errorlevel%
-	
-	:printHelp
-	echo %~nx0 prints the value (or list of values)
-	echo       of xml attribute/node in xml by given xpath expression
-	echo(
-	echo Usage:
-	echo(
-	echo call %~nx0 "filePath" "xpathExpression"
-	exit /b %errorlevel%
+
+    :printHelp
+    echo %~nx0 prints the value (or list of values)
+    echo       of xml attribute/node in xml by given xpath expression
+    echo(
+    echo Usage:
+    echo(
+    echo call %~nx0 "filePath" "xpathExpression"
+    exit /b %errorlevel%
 
 @if (@X)==(@Y) @end JScript comment */
 
 if (WScript.Arguments.length<2)
 {
-	WScript.Echo("Not enough arguments");
-	WScript.Quit(3);
+    WScript.Echo("Not enough arguments");
+    WScript.Quit(3);
 }
 
 
@@ -61,42 +61,42 @@ var objNodes;
 var loaded;
 
 try {
-	objDoc = WScript.CreateObject("MSXML.DOMDocument");
-	loaded=objDoc.load(WScript.Arguments.Named.Item("file"));
+    objDoc = WScript.CreateObject("MSXML.DOMDocument");
+    loaded=objDoc.load(WScript.Arguments.Named.Item("file"));
 } catch (err){
-	WScript.Echo("Error while parsing the xml");
-	WScript.Echo(err.message);
-	WScript.Quit(1);
+    WScript.Echo("Error while parsing the xml");
+    WScript.Echo(err.message);
+    WScript.Quit(1);
 }
 
 if(!loaded){
-	WScript.Echo("Error while parsing the xml");
-	WScript.Echo("");
-	WScript.Echo("Error Code:"+objDoc.parseError.errorCode);
-	WScript.Echo("");
-	WScript.Echo("Line:"+objDoc.parseError.line+" Posotion:"+objDoc.parseError.filepos);
-	WScript.Echo("");
-	WScript.Echo("Reason:"+objDoc.parseError.reason);
-	WScript.Echo("");
-	WScript.Echo("URL:"+objDoc.parseError.url);
-	WScript.Echo("");
-	WScript.Echo(objDoc.parseError.srcText);
-	WScript.Quit(5);
+    WScript.Echo("Error while parsing the xml");
+    WScript.Echo("");
+    WScript.Echo("Error Code:"+objDoc.parseError.errorCode);
+    WScript.Echo("");
+    WScript.Echo("Line:"+objDoc.parseError.line+" Posotion:"+objDoc.parseError.filepos);
+    WScript.Echo("");
+    WScript.Echo("Reason:"+objDoc.parseError.reason);
+    WScript.Echo("");
+    WScript.Echo("URL:"+objDoc.parseError.url);
+    WScript.Echo("");
+    WScript.Echo(objDoc.parseError.srcText);
+    WScript.Quit(5);
 }
 
 try {
-	var objNodes = objDoc.selectNodes(xp);
+    var objNodes = objDoc.selectNodes(xp);
 } catch (err){
-	WScript.Echo("invalid xpath expression");
-	WScript.Echo(err.message);
-	WScript.Quit(2);
+    WScript.Echo("invalid xpath expression");
+    WScript.Echo(err.message);
+    WScript.Quit(2);
 }
 
 for (var i=0;i<objNodes.length;i++){
     if(option.toLowerCase()=="xml"){
-		WScript.Echo(objNodes.item(i).xml);
-	} else {
-		WScript.Echo(objNodes.item(i).text);
-	}
-	
+        WScript.Echo(objNodes.item(i).xml);
+    } else {
+        WScript.Echo(objNodes.item(i).text);
+    }
+
 }
