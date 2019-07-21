12a35d96aea7beeef03c64bafb677895ff47f36b
Amir <amkad87@gmail.com>
2019-07-21 08:17:46 +0300

    add relinker https://github.com/KeepSafe/ReLinker for load native libraries on Android

--- framework/core/android/wrapper/AkkordActivity.java
+++ framework/core/android/wrapper/AkkordActivity.java
@@ -3,10 +3,37 @@ package org.akkord.lib;
 import org.libsdl.app.SDLActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import com.getkeepsafe.relinker.ReLinker;
 
 public class AkkordActivity extends SDLActivity {
 
     //protected static AkkordActivity instance;
+
+    public void akkord_loadLibrary(String libraryName) throws UnsatisfiedLinkError, SecurityException, NullPointerException {
+
+        if (libraryName == null) {
+            throw new NullPointerException("No library name provided.");
+        }
+
+        try {
+            ReLinker.loadLibrary(this, libraryName);
+            Log.d("SDL", "Relinker library load success: " + libraryName);
+        }
+        catch (final Throwable e) {
+            // Fall back
+            try {
+                Log.e("SDL", "Relinker ERROR!!! " + e.getMessage());
+                System.loadLibrary(libraryName);
+            }
+            catch (final UnsatisfiedLinkError ule) {
+                throw ule;
+            }
+            catch (final SecurityException se) {
+                throw se;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +54,13 @@ public class AkkordActivity extends SDLActivity {
             // "SDL2_ttf",
             "main"
         };
+    }
+
+    @Override
+    public void loadLibraries() {
+       for (String lib : getLibraries()) {
+          //System.loadLibrary(lib);
+          akkord_loadLibrary(lib);
+       }
     }
 }
