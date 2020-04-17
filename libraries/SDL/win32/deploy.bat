34db0c5704b59a7530630a6ea7fdd63c680feb25
Amir <amkad87@gmail.com>
2020-04-17 13:45:16 +0300

    deploy.bat

---
+++ libraries/SDL/win32/deploy.bat
@@ -0,0 +1,6 @@
+del /q Release\*
+set source_path=..\SDL\VisualC\SDL\Win32\Release
+copy %source_path%\SDL2.dll Release\
+copy %source_path%\SDL2.lib Release\
+copy %source_path%\SDL2.pdb Release\
+copy %source_path%\SDL2main.dll Release\
