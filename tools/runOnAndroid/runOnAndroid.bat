287ff07f1a4a018e3827c2288320be97f8bb8826
Amir <amkad87@gmail.com>
2020-05-04 14:40:47 +0300

    Убираем отладочный вывод в консоль

--- tools/runOnAndroid/runOnAndroid.bat
+++ tools/runOnAndroid/runOnAndroid.bat
@@ -3,30 +3,22 @@ rem %2 - activityName
 rem %3 - 0 or 1 if we need start logcat
 
 FOR /F "skip=1 delims=~" %%x IN ('adb devices -l') DO (
-    echo %%x
     for /F "tokens=5* delims=:" %%a in ("%%x") do (
-        echo %%a
-        DO call adb -t %%a logcat -c
+        call adb -t %%a logcat -c
     )
 )
 
 FOR /F "skip=1 delims=~" %%x IN ('adb devices -l') DO (
-    echo %%x
     for /F "tokens=5* delims=:" %%a in ("%%x") do (
-        echo %%a
-        DO call adb -t %%a shell am start -n %1/%1.%2
+        call adb -t %%a shell am start -n %1/%1.%2
     )
 )
 
-FOR /F "skip=1 delims=~" %%x IN ('adb devices -l') DO (
-    for /F "tokens=2,3,4,5 delims=:" %%a in ("%%x") do (
-        echo %%a
-        echo %%b
-        echo %%c
-        echo %%d
 
-        set str = "%%a:%%b:%%c:%%d"
-        echo %str%
-        start %0/../runlogcat.bat %%d
+if 1 == %3 (
+    FOR /F "skip=1 delims=~" %%x IN ('adb devices -l') DO (
+        for /F "tokens=5 delims=:" %%a in ("%%x") do (
+            start %0/../runlogcat.bat %%a %%x
+        )
     )
 )
