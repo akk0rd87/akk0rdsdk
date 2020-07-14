646867e9f19a6ef04806e0d99d1d0719a14c9ebe
akadyrov <akadyrov@sportmaster.ru>
2020-07-14 12:51:08 +0300

    оптимизация configmanager

--- framework/configmanager.cpp
+++ framework/configmanager.cpp
@@ -1,54 +1,38 @@
 #include <fstream>
 #include "configmanager.h"
 
-std::vector<ConfigManager::ConfigStruct>::iterator ConfigManager::GetConfigIt(const char* Key) {
-    auto it = std::find_if(ConfigVector.begin(), ConfigVector.end(), [&Key](const ConfigStruct& CFG) {
-        if (CFG.Key == std::string(Key))
-            return true;
-        return false;
-    });
-    return it;
-};
-
 void ConfigManager::SetValue(const char* Key, const char* Value)
 {
     auto it = GetConfigIt(Key);
-    if (it != ConfigVector.cend())
-    {
-        it->Value = std::string(Value);
+    if (it != ConfigList.cend()) {
+        it->second = Value;
     }
-    else
-    {
-        ConfigVector.emplace_back(Key, Value);
+    else {
+        ConfigList.emplace(Key, Value);
     }
 }
 
-bool ConfigManager::Load()
-{
-    this->Destroy();
+bool ConfigManager::Load() {
+    this->Clear();
     FileReader fr;
-    if (fr.Open(FileName.c_str(), FSearchPriority))
-    {
+    if (fr.Open(FileName.c_str(), FSearchPriority)) {
         std::string line;
         decltype(line.find('=')) pos = 0;
-        while (fr.ReadLine(line))
-        {
-            if (line.size() > 0)
-            {
+        while (fr.ReadLine(line)) {
+            if (!line.empty()) {
                 pos = line.find('=');
-                if (pos != std::string::npos)
-                {
-                    ConfigVector.emplace_back(std::string(line, 0, pos).c_str(), std::string(line, pos + 1).c_str());
+                if (pos != std::string::npos) {
+                    ConfigList.emplace(std::string(line, 0, pos).c_str(), std::string(line, pos + 1).c_str());
                 }
             }
         }
         fr.Close();
         return true;
     }
-    else
-    {
-        if (BWrapper::FileSearchPriority::Assets == FSearchPriority)
+    else {
+        if (BWrapper::FileSearchPriority::Assets == FSearchPriority) {
             logError("ConfigManager::Load(): Asset config file not found %s", this->FileName.c_str());
+        }
     }
     fr.Close();
 
@@ -58,98 +42,25 @@ bool ConfigManager::Load()
 
 bool ConfigManager::Save()
 {
-    if (BWrapper::FileSearchPriority::Assets == FSearchPriority)
-    {
+    if (BWrapper::FileSearchPriority::Assets == FSearchPriority) {
         logError("ConfigManager::Save(): Asset config file save is not allowed %s", this->FileName.c_str());
         return false;
     }
 
-    {// создаем директорию для файла, если она еще не существует
-        decltype(FileName.rfind("/")) s1 = this->FileName.rfind("/");
-        decltype(FileName.rfind("/")) s2 = this->FileName.rfind("\\");
-
-        decltype(s1) res = 0;
-
-        if (s1 != std::string::npos)
-            res = s1;
-
-        if (s2 != std::string::npos && res < s2)
-            res = s2;
-
-        if (res)
-        {
-            BWrapper::DirCreate(std::string(this->FileName, 0, res).c_str());
-        }
-    }
+    BWrapper::DirCreate(this->FileName.substr(0, this->FileName.find_last_of("\\/")).c_str());
 
     std::ofstream ofs(this->FileName.c_str(), std::ofstream::binary | std::ofstream::out);
-    for (const auto& v : ConfigVector)
-        ofs << v.Key << "=" << v.Value << "\n";
+    for (const auto& v : ConfigList) {
+        ofs << v.first << "=" << v.second << "\n";
+    }
 
     ofs.close();
     return true;
 }
 
-bool ConfigManager::SetIntValue(const char* Key, int Value)
-{
-    SetValue(Key, std::to_string(Value).c_str());
-    return true;
-}
-
-bool ConfigManager::SetStrValue(const char* Key, const char* Value)
-{
-    SetValue(Key, Value);
-    return true;
-}
-
-int  ConfigManager::GetIntValue(const char* Key, int DefaultValue)
-{
-    auto it = GetConfigIt(Key);
-    if (it != ConfigVector.end())
-    {
-        return std::stoi(it->Value);
-    }
-    else
-    {
-        return DefaultValue;
-    }
-}
-std::string ConfigManager::GetStrValue(const char* Key, const char* DefaultValue)
-{
-    auto it = GetConfigIt(Key);
-    if (it != ConfigVector.end())
-    {
-        return it->Value;
-    }
-    else
-    {
-        return DefaultValue;
-    }
-}
-
-ConfigManager::ConfigManager()
-{
-    this->Destroy();
-}
-
-ConfigManager::~ConfigManager()
-{
-    this->Destroy();
-}
-
-void ConfigManager::Destroy()
-{
-    this->Clear();
-}
-
-void ConfigManager::Clear()
-{
-    ConfigVector.clear();
-}
-
-void ConfigManager::PrintConfig()
-{
+void ConfigManager::PrintConfig() {
     logInfo("=== ConfigManager::PrintConfig() ===");
-    for (decltype(ConfigVector.size()) i = 0; i < ConfigVector.size(); i++)
-        logInfo("ConfigManager::PrintConfig(): File=%s, Key=[%s] Value=[%s]", this->FileName.c_str(), ConfigVector[i].Key.c_str(), ConfigVector[i].Value.c_str());
+    for (const auto& v : ConfigList) {
+        logDebug("File=%s, Key=[%s] Value=[%s]", this->FileName.c_str(), v.first.c_str(), v.second.c_str());
+    }
 }
--- framework/configmanager.h
+++ framework/configmanager.h
@@ -2,6 +2,7 @@
 #ifndef __AKK0RD_CONFIG_MANAGER_H__
 #define __AKK0RD_CONFIG_MANAGER_H__
 
+#include <unordered_map>
 #include "basewrapper.h"
 
 // Класс управления настройками в виде списка Key-Value
@@ -10,51 +11,36 @@ class ConfigManager
 public:
     bool         Load();
     bool         Save();
-    void         Clear();
+    void         Clear() { ConfigList.clear(); };
 
     void         SetFile(const std::string& FileName, BWrapper::FileSearchPriority SearchPriority) { this->FileName = FileName; this->FSearchPriority = SearchPriority; };
 
-    bool         SetIntValue(const char* Key, int         Value);
-    bool         SetStrValue(const char* Key, const char* Value);
+    bool         SetIntValue(const char* Key, int         Value) { SetValue(Key, std::to_string(Value).c_str()); return true; };
+    bool         SetStrValue(const char* Key, const char* Value) { SetValue(Key, Value); return true; };
 
-    int          GetIntValue(const char* Key, int         DefaultValue);
-    std::string  GetStrValue(const char* Key, const char* DefaultValue);
+    int          GetIntValue(const char* Key, int         DefaultValue) { const auto it = GetConfigIt(Key); return (it != ConfigList.end() ? std::stoi(it->second) : DefaultValue); };
+    std::string  GetStrValue(const char* Key, const char* DefaultValue) { const auto it = GetConfigIt(Key); return (it != ConfigList.end() ? it->second : DefaultValue); };
 
     void         PrintConfig();
 
-    ConfigManager();
-    ~ConfigManager();
-
+    ConfigManager() {};
+    ~ConfigManager() {};
 private:
-    class ConfigStruct
-    {
-    public:
-        std::string Key;
-        std::string Value;
-        ConfigStruct() {}
-        ConfigStruct(const char* Key, const char* Value) : Key(Key), Value(Value) {}
-        ~ConfigStruct()
-        {
-            Key.clear();
-            Value.clear();
-        }
-    };
-
     // список пар Key-Value
-    std::vector<ConfigStruct> ConfigVector;
+    std::unordered_map<std::string, std::string> ConfigList;
     void SetValue(const char* Key, const char* Value);
 
     BWrapper::FileSearchPriority FSearchPriority = BWrapper::FileSearchPriority::FileSystem;
     std::string FileName;
-    void Destroy();
 
-    //Запрещаем создавать экземпляр класса ConfigManager
     ConfigManager(const ConfigManager& rhs) = delete; // Копирующий: конструктор
-    ConfigManager(ConfigManager&& rhs) = delete; // Перемещающий: конструктор
+    ConfigManager(ConfigManager&& rhs) = default; // Перемещающий: конструктор
     ConfigManager& operator= (const ConfigManager& rhs) = delete; // Оператор копирующего присваивания
-    ConfigManager& operator= (ConfigManager&& rhs) = delete; // Оператор перемещающего присваивания
+    ConfigManager& operator= (ConfigManager&& rhs) = default; // Оператор перемещающего присваивания
 
-    std::vector<ConfigStruct>::iterator GetConfigIt(const char* Key);
+    decltype(ConfigList)::iterator GetConfigIt(const char* Key) {
+        return ConfigList.find(Key);
+    };
 };
 
 #endif // __AKK0RD_CONFIG_MANAGER_H__
