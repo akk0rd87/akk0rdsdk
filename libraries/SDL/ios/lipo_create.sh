5d9c85ddc43036fc8cd76139c900dcb9ae6ddff9
Amir Kadyrov [macos] <amkad87@yandex.ru>
2020-10-17 11:20:30 +0300

    build scripts

--- libraries/SDL/ios/lipo_create.sh
+++ libraries/SDL/ios/lipo_create.sh
@@ -1,4 +1,5 @@
 rm libSDL2.a
 xcodebuild -project ../SDL/Xcode-iOS/SDL/SDL.xcodeproj -target libSDL-iOS -configuration Release -sdk iphoneos
-xcodebuild -project ../SDL/Xcode-iOS/SDL/SDL.xcodeproj -target libSDL-iOS -configuration Release -sdk iphonesimulator
-lipo -create ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2.a ../SDL/Xcode-iOS/SDL/build/Release-iphonesimulator/libSDL2.a -output libSDL2.a
+#xcodebuild -project ../SDL/Xcode-iOS/SDL/SDL.xcodeproj -target libSDL-iOS -configuration Release -sdk iphonesimulator
+#lipo -create ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2.a ../SDL/Xcode-iOS/SDL/build/Release-iphonesimulator/libSDL2.a -output libSDL2.a
+cp ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2.a -output libSDL2.a
--- libraries/SDL_image/ios/lipo_create.sh
+++ libraries/SDL_image/ios/lipo_create.sh
@@ -1,4 +1,5 @@
 rm libSDL2_image.a
 xcodebuild -project ../SDL_Image/Xcode-iOS/SDL_image.xcodeproj -target libSDL_image-iOS -configuration Release -sdk iphoneos
-xcodebuild -project ../SDL_Image/Xcode-iOS/SDL_image.xcodeproj -target libSDL_image-iOS -configuration Release -sdk iphonesimulator
-lipo -create ../SDL_Image/Xcode-iOS/build/Release-iphoneos/libSDL2_image.a ../SDL_Image/Xcode-iOS/build/Release-iphonesimulator/libSDL2_image.a -output libSDL2_image.a
+#xcodebuild -project ../SDL_Image/Xcode-iOS/SDL_image.xcodeproj -target libSDL_image-iOS -configuration Release -sdk iphonesimulator
+#lipo -create ../SDL_Image/Xcode-iOS/build/Release-iphoneos/libSDL2_image.a ../SDL_Image/Xcode-iOS/build/Release-iphonesimulator/libSDL2_image.a -output libSDL2_image.a
+cp ../SDL_Image/Xcode-iOS/build/Release-iphoneos/libSDL2_image.a libSDL2_image.a
