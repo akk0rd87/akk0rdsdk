4044a88dd1ab61be11391154100cb3ce6530f704
Amir <amkad87@gmail.com>
2020-11-06 17:03:02 +0300

    AdRandomizer::Apps::CJCross

--- framework/adrandomizer.h
+++ framework/adrandomizer.h
@@ -6,7 +6,7 @@
 class AdRandomizer
 {
 public:
-    enum struct Apps : unsigned { CFCross, FCross, JCross, JDraw, WordsRu1, WordsRu2, WordsRu8, WordsUs, Sudoku, DotLines, PirateBomb /*, CJCross*/ };
+    enum struct Apps : unsigned { CFCross, FCross, JCross, JDraw, WordsRu1, WordsRu2, WordsRu8, WordsUs, Sudoku, DotLines, PirateBomb, CJCross };
     static void    OpenURL(Apps AppId);
     static void    OpenURL(BWrapper::OS OSCode, Apps AppId);
     static void    OpenPublisherAppstorePage();
@@ -14,7 +14,7 @@ public:
     //Запрещаем создавать экземпляр класса AdRandomizer
     AdRandomizer() = delete;
     ~AdRandomizer() = delete;
-    AdRandomizer(const AdRandomizer& rhs)  = delete; // Копирующий: конструктор
+    AdRandomizer(const AdRandomizer& rhs) = delete; // Копирующий: конструктор
     AdRandomizer(AdRandomizer&& rhs) = delete; // Перемещающий: конструктор
     AdRandomizer& operator= (const AdRandomizer& rhs) = delete; // Оператор копирующего присваивания
     AdRandomizer& operator= (AdRandomizer&& rhs) = delete; // Оператор перемещающего присваивания
