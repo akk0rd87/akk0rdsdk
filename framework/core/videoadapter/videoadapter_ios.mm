6886ba8e7d137ed51af05ff39d9ed978515bc602
Amir <amkad87@gmail.com>
2020-11-21 11:31:34 +0300

    для ios не требуется загрузка openglES функций

--- framework/core/videoadapter/videoadapter_ios.mm
+++ framework/core/videoadapter/videoadapter_ios.mm
@@ -1,6 +1,4 @@
-#define __AKKORD_SDK_GETGLESPROCADDR__ // требуется явно получать адреса GLES-функций
 #include "video_opengles.h"
-#undef __AKKORD_SDK_GETGLESPROCADDR__
 #include "video_metal.h"
 
 static VideoAdapter_OPENGLES glesAdapter;
