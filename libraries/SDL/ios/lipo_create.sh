80aab8fa48df6a5def54fbf3a535b1c4bb054319
Amir <amkad87@gmail.com>
2021-03-18 19:11:26 +0300

    SDL ios build script correction

--- libraries/SDL/ios/lipo_create.sh
+++ libraries/SDL/ios/lipo_create.sh
@@ -1,5 +1,8 @@
 rm libSDL2.a
-xcodebuild -project ../SDL/Xcode-iOS/SDL/SDL.xcodeproj -target libSDL-iOS -configuration Release -sdk iphoneos
+#xcodebuild -project ../SDL/Xcode-iOS/SDL/SDL.xcodeproj -target libSDL-iOS -configuration Release -sdk iphoneos
 #xcodebuild -project ../SDL/Xcode-iOS/SDL/SDL.xcodeproj -target libSDL-iOS -configuration Release -sdk iphonesimulator
 #lipo -create ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2.a ../SDL/Xcode-iOS/SDL/build/Release-iphonesimulator/libSDL2.a -output libSDL2.a
-cp ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2.a libSDL2.a
+#cp ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2.a libSDL2.a
+
+xcodebuild -project ../SDL/Xcode/SDL/SDL.xcodeproj -target "Static Library-iOS" -configuration Release -sdk iphoneos
+cp ../SDL/Xcode/SDL/build/Release-iphoneos/libSDL2.a libSDL2.a
