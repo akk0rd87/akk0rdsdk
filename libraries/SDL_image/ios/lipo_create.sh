0c2a2092ff274e5949743c7283bda9df7254ee6d
Amir Kadyrov [macos] <amkad87@yandex.ru>
2021-03-18 21:01:22 +0200

    sdl_image xcodebuild

--- libraries/SDL_image/ios/lipo_create.sh
+++ libraries/SDL_image/ios/lipo_create.sh
@@ -1,5 +1,5 @@
 rm libSDL2_image.a
-xcodebuild -project ../SDL_Image/Xcode-iOS/SDL_image.xcodeproj -target libSDL_image-iOS -configuration Release -sdk iphoneos
-#xcodebuild -project ../SDL_Image/Xcode-iOS/SDL_image.xcodeproj -target libSDL_image-iOS -configuration Release -sdk iphonesimulator
+xcodebuild -project ../SDL_Image/Xcode-iOS/SDL_image.xcodeproj -target libSDL_image-iOS -configuration Release -sdk iphoneos HEADER_SEARCH_PATHS=../../../SDL/SDL/include
+
 #lipo -create ../SDL_Image/Xcode-iOS/build/Release-iphoneos/libSDL2_image.a ../SDL_Image/Xcode-iOS/build/Release-iphonesimulator/libSDL2_image.a -output libSDL2_image.a
 cp ../SDL_Image/Xcode-iOS/build/Release-iphoneos/libSDL2_image.a libSDL2_image.a
