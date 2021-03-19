c5b29db71ccacc538ad55808efb10fece9d7fc5f
Amir <amkad87@gmail.com>
2021-03-19 21:49:32 +0300

    ios Admob 8.2.0

--- framework/core/platformwrapper/ios/ios_admob.mm
+++ framework/core/platformwrapper/ios/ios_admob.mm
@@ -1,345 +1,261 @@
-//#include "SDL.h"
 #include "ios_admob.h"
 #include "admob.h"
 #import <GoogleMobileAds/GoogleMobileAds.h>
 
-@interface iAdMob : NSObject
-+ (iAdMob*) defaultAdMob;
--(void)Init;
--(void)InterstitialSetUnitID:(const char*) UnitID;
--(void)InterstitialLoad;
--(void)InterstitialShow;
--(void)SetEventCallback:(AdMob::AdEventCallback*) EventCallback;
+AdMob::AdEventCallback *admobCallback = nullptr;
+static void sendCallback(const AdMob::AdEvent& CallbackEvent) {
+    if(admobCallback) {
+        admobCallback(&CallbackEvent);
+    }
+    else {
+        logError("iOS Admob Callback is not set");
+    }
+};
 
--(void)RewardedVideoSetUnitID:(const char*) UnitID;
--(void)RewardedVideoLoad;
--(void)RewardedVideoShow;
+/*
+    INTERSTITIAL CALLBACKS
+*/
+@interface iInterstitial : NSObject
++(iInterstitial*) defaultInterstitial;
 @end
 
-@interface iAdMob() <GADInterstitialDelegate, GADRewardedAdDelegate>
--(void)InterstitialDestroy;
--(void)InterstitialReset;
--(void)SendCallback:(const    AdMob::AdEvent*) Event;
-@property (atomic, strong)    GADInterstitial          *interstitial;
-@property (atomic, strong)    GADRewardedAd            *rewardedAd;
-@property (nonatomic)         AdMob::AdEventCallback   *Callback;
-@property (nonatomic)         std::string               InterstitialUnitID;
-@property (nonatomic)         std::string               RewardedVideoUnitID;
+@interface iInterstitial() <GADFullScreenContentDelegate>
+@property(nonatomic, strong) GADInterstitialAd *interstitial;
+@property(nonatomic)         std::string        UnitID;
+-(void)SetUintId:(const char* )ID;
+-(void)Load;
+-(void)Show;
 @end
 
-@implementation iAdMob
-+ (iAdMob *) defaultAdMob
+@implementation iInterstitial
+
++ (iInterstitial *) defaultInterstitial
 {
-    static iAdMob* adMob;
+    static iInterstitial* adMob;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        if (!adMob)
-        {
-            adMob = [[iAdMob alloc] init];
+        if (!adMob) {
+            adMob = [[iInterstitial alloc] init];
         }
     });
     return adMob;
 }
 
--(void)SendCallback:(const AdMob::AdEvent*) Event
-{
-    if(self.Callback != nullptr)
-    {
-        self.Callback(Event);
+-(void)SetUintId:(const char* )ID {
+    self.UnitID = ID;
+};
+
+- (void)Load {
+    GADRequest *request = [GADRequest request];
+    if(request) {
+        @autoreleasepool {
+            NSString *ID = [[NSString alloc] initWithUTF8String:self.UnitID.c_str()];
+            [GADInterstitialAd loadWithAdUnitID:ID
+                                        request:request
+                              completionHandler:^(GADInterstitialAd *ad, NSError *error) {
+                AdMob::AdEvent Ad;
+                Ad.AdFormat = static_cast<decltype(Ad.AdFormat)>(AdMob::Format::Interstitial);
+                using event = AdMob::InterstitialEvent;
+              if (error) {
+                  logDebug("InterstitialLoad success load");
+                  //NSLog(@"Failed to load interstitial ad with error: %@", [error localizedDescription]);
+                  Ad.EventType = static_cast<decltype(Ad.EventType)>(event::Failed);
+              }
+              else {
+                  Ad.EventType = static_cast<decltype(Ad.EventType)>(event::Loaded);
+                  logDebug("InterstitialLoad success load");
+                  self.interstitial = ad;
+                  self.interstitial.fullScreenContentDelegate = self;
+              }
+              sendCallback(Ad);
+            }];
+        }
     }
-    else
-    {
-        logError("Ad Event Callback is empty");
-    }
-};
-
-
-////
-//// INTERSTITIAL DELEGATE CALLBACKS
-////
-
-- (void)interstitialDidReceiveAd:(nonnull GADInterstitial *)ad
-{
-    logDebug("interstitialDidReceiveAd");
-    AdMob::AdEvent Ad;
-    Ad.AdFormat = (int)AdMob::Format::Interstitial;
-    Ad.EventType = (int)AdMob::InterstitialEvent::Loaded;
-    [self SendCallback: &Ad];
-};
-
-- (void)interstitial:(nonnull GADInterstitial *)ad didFailToReceiveAdWithError:(nonnull GADRequestError *)error
-{
-    logDebug("didFailToReceiveAdWithError");
-    AdMob::AdEvent Ad;
-    Ad.AdFormat = (int)AdMob::Format::Interstitial;
-    Ad.EventType = (int)AdMob::InterstitialEvent::Failed;
-    [self SendCallback: &Ad];
-};
-
-- (void)interstitialWillPresentScreen:(nonnull GADInterstitial *)ad
-{
-    // Do nothing
-    logDebug("interstitialWillPresentScreen");
-};
-
-- (void)interstitialDidFailToPresentScreen:(nonnull GADInterstitial *)ad
-{
-    logDebug("interstitialDidFailToPresentScreen");
-    AdMob::AdEvent Ad;
-    Ad.AdFormat = (int)AdMob::Format::Interstitial;
-    Ad.EventType = (int)AdMob::InterstitialEvent::Failed;
-    [self SendCallback: &Ad];
-};
-
-- (void)interstitialWillDismissScreen:(nonnull GADInterstitial *)ad
-{
-    // Do nothing
-    logDebug("interstitialWillDismissScreen");
-};
-
-- (void)interstitialDidDismissScreen:(nonnull GADInterstitial *)ad
-{
-    logDebug("interstitialDidDismissScreen");
-    AdMob::AdEvent Ad;
-    Ad.AdFormat = (int)AdMob::Format::Interstitial;
-    Ad.EventType = (int)AdMob::InterstitialEvent::Closed;
-    [self SendCallback: &Ad];
-};
-
-- (void)interstitialWillLeaveApplication:(nonnull GADInterstitial *)ad
-{
-    logDebug("interstitialWillLeaveApplication");
-    AdMob::AdEvent Ad;
-    Ad.AdFormat = (int)AdMob::Format::Interstitial;
-    Ad.EventType = (int)AdMob::InterstitialEvent::LeftApplication;
-    [self SendCallback: &Ad];
-};
-
-///////
-///////  REWARDED VIDEO DELEGATE CALLBACKS
-///////
-
-- (void)rewardedAd:(GADRewardedAd *)rewardedAd userDidEarnReward:(GADAdReward *)reward {
-    logDebug("Rewarded");
-    AdMob::AdEvent Ad;
-    Ad.AdFormat = (int)AdMob::Format::RewardedVideo;
-    Ad.EventType = (int)AdMob::RewardedVideoEvent::Rewarded;
-    [self SendCallback: &Ad];
 }
 
-- (void)rewardedAdDidPresent:(GADRewardedAd *)rewardedAd {
-    logDebug("Opened reward based video ad.");
-    AdMob::AdEvent Ad;
-    Ad.AdFormat = (int)AdMob::Format::RewardedVideo;
-    Ad.EventType = (int)AdMob::RewardedVideoEvent::Opened;
-    [self SendCallback: &Ad];
-}
-
-
-- (void)rewardedAdDidDismiss:(GADRewardedAd *)rewardedAd {
-    logDebug("Reward based video ad is dismissed.");
-    AdMob::AdEvent Ad;
-    Ad.AdFormat = (int)AdMob::Format::RewardedVideo;
-    Ad.EventType = (int)AdMob::RewardedVideoEvent::Closed;
-    [self SendCallback: &Ad];
-}
-
-- (void)rewardedAd:(GADRewardedAd *)rewardedAd didFailToPresentWithError:(NSError *)error {
-    logDebug("Reward ad didFailToPresentWithError");
-//    AdMob::AdEvent Ad;
-//    Ad.AdFormat = (int)AdMob::Format::RewardedVideo;
-//    Ad.EventType = (int)AdMob::RewardedVideoEvent::LeftApplication;
-//    [self SendCallback: &Ad];
-}
-
-// interface API
-
--(void)Init
-{
-    [[GADMobileAds sharedInstance] startWithCompletionHandler:nil];
-    self.interstitial = nullptr;
-    self.rewardedAd = nullptr;
-    self.Callback = nullptr;
-//    [GADRewardBasedVideoAd sharedInstance].delegate = self;
-};
-
--(void)InterstitialDestroy
-{
-    if(self.interstitial != nullptr)
-    {
-        [self.interstitial release];
-        self.interstitial = nullptr;
-    }
-};
-
--(void)InterstitialReset
-{
-    [self InterstitialDestroy];
-    NSString *ID = [[NSString alloc] initWithUTF8String:self.InterstitialUnitID.c_str()];
-    self.interstitial = [[GADInterstitial alloc] initWithAdUnitID:ID];
-    self.interstitial.delegate = self;
-    [ID release];
-}
-
--(void)SetEventCallback:(AdMob::AdEventCallback*) EventCallback
-{
-    self.Callback = EventCallback;
-};
-
--(void)InterstitialSetUnitID:(const char*) UnitID
-{
-    logDebug((std::string("InterstitialSetUnitID: ") + UnitID).c_str());
-    self.InterstitialUnitID = std::string(UnitID);
-    [self InterstitialReset];
-};
-
--(void)InterstitialLoad
-{
-    logDebug("InterstitialLoad 1");
-    if(self.interstitial != nullptr)
-    {
-       logDebug("InterstitialLoad 2");
-
-       if(self.interstitial.hasBeenUsed)
-       {
-           [self InterstitialReset];
-       }
-
-       GADRequest *request = [GADRequest request];
-       //request.testDevices = @[ kGADSimulatorID ];
-       [self.interstitial loadRequest:request];
-    }
-    else
-    {
-        logError("Interstitial is empty");
-    }
-};
-
--(void)InterstitialShow
-{
-    logDebug("InterstitialShow 1");
-    if(self.interstitial != nullptr)
-    {
-        logDebug("InterstitialShow 2");
-        if([self.interstitial isReady])
-        {
-            logDebug("InterstitialShow 3");
-            //https://stackoverflow.com/questions/12418177/how-to-get-root-view-controller
-            UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
+-(void)Show {
+    if (self.interstitial) {
+        UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
+        if ([self.interstitial canPresentFromRootViewController :controller error:nil]) {
             [self.interstitial presentFromRootViewController:controller];
         }
-        else
-        {
-            logWarning("Interstitial is not ready for present");
-        }
-    }
-    else
-    {
-        logError("Interstitial is empty");
     }
 };
 
--(void)RewardedVideoSetUnitID:(const char*) UnitID
-{
-    self.RewardedVideoUnitID = std::string(UnitID);
-};
+/// Tells the delegate that the ad failed to present full screen content.
+- (void)ad:(nonnull id<GADFullScreenPresentingAd>)ad
+didFailToPresentFullScreenContentWithError:(nonnull NSError *)error {
+    logDebug("Ad did fail to present full screen content.");
+    AdMob::AdEvent Ad;
+    Ad.AdFormat = static_cast<decltype(Ad.AdFormat)>(AdMob::Format::Interstitial);
+    Ad.EventType = static_cast<decltype(Ad.EventType)>(AdMob::InterstitialEvent::Failed);
+    sendCallback(Ad);
+}
 
--(void)RewardedVideoLoad
-{
-//    NSString *ID = [[NSString alloc] initWithUTF8String:self.RewardedVideoUnitID.c_str()];
-//    [[GADRewardBasedVideoAd sharedInstance] loadRequest:[GADRequest request] withAdUnitID:ID];
-//    [ID release];
-    if(self.rewardedAd) {
-        [self.rewardedAd release];
-        self.rewardedAd = nullptr;
-    };
+/// Tells the delegate that the ad presented full screen content.
+- (void)adDidPresentFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
+    logDebug("Ad did present full screen content.");
+}
 
-    NSString *ID = [[NSString alloc] initWithUTF8String:self.RewardedVideoUnitID.c_str()];
-    self.rewardedAd = [[GADRewardedAd alloc] initWithAdUnitID:ID];
+/// Tells the delegate that the ad dismissed full screen content.
+- (void)adDidDismissFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
+    logDebug("Ad did dismiss full screen content.");
+    AdMob::AdEvent Ad;
+    Ad.AdFormat = static_cast<decltype(Ad.AdFormat)>(AdMob::Format::Interstitial);
+    Ad.EventType = static_cast<decltype(Ad.EventType)>(AdMob::InterstitialEvent::Closed);
+    sendCallback(Ad);
+}
 
-    if(self.rewardedAd) {
-        GADRequest *request = [GADRequest request];
-        [self.rewardedAd loadRequest:request completionHandler:^(GADRequestError * _Nullable error) {
-          AdMob::AdEvent Ad;
-          Ad.AdFormat = static_cast<int>(AdMob::Format::RewardedVideo);
-          if (error) {
-            // Handle ad failed to load case.
-              logDebug("Rewarded vided load failed");
-              Ad.EventType = static_cast<int>(AdMob::RewardedVideoEvent::Failed);
-          } else {
-            // Ad successfully loaded.
-              logDebug("Rewarded vided ad successfully loaded");
-              Ad.EventType = static_cast<int>(AdMob::RewardedVideoEvent::Loaded);
-          };
-          [self SendCallback: &Ad];
-        }];
-    }
-};
-
--(void)RewardedVideoShow
-{
-    /*
-    if ([[GADRewardBasedVideoAd sharedInstance] isReady])
-    {
-        //https://stackoverflow.com/questions/12418177/how-to-get-root-view-controller
-        UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
-        [[GADRewardBasedVideoAd sharedInstance] presentFromRootViewController:controller];
-    }
-     */
-    if(self.rewardedAd) {
-        if(self.rewardedAd.isReady) {
-            UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
-            [self.rewardedAd presentFromRootViewController:controller delegate:self];
-        }
-    }
-};
-
+- (void)adDidRecordImpression:(nonnull id<GADFullScreenPresentingAd>)ad {
+    logDebug("adDidRecordImpression");
+}
 @end
 
+/*
+    REWARDED CALLBACKS
+*/
+@interface iRewardedVideo : NSObject
++(iRewardedVideo*) defaultRewardedVideo;
+@end
 
-bool AdMobiOS::Init(const char* AdMobAppID, int Formats)
+@interface iRewardedVideo() <GADFullScreenContentDelegate>
+@property(nonatomic, strong) GADRewardedAd *rewardedAd;
+@property(nonatomic)         std::string        UnitID;
+-(void)SetUintId:(const char* )ID;
+-(void)Load;
+-(void)Show;
+@end
+
+@implementation iRewardedVideo
+
++ (iRewardedVideo *) defaultRewardedVideo
 {
-    [[iAdMob defaultAdMob] Init];
+    static iRewardedVideo* adMob;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        if (!adMob) {
+            adMob = [[iRewardedVideo alloc] init];
+        }
+    });
+    return adMob;
+}
+
+-(void)SetUintId:(const char* )ID {
+    self.UnitID = ID;
+};
+
+- (void)Load {
+    GADRequest *request = [GADRequest request];
+    if(request) {
+        @autoreleasepool {
+            NSString *ID = [[NSString alloc] initWithUTF8String:self.UnitID.c_str()];
+            [GADRewardedAd
+                 loadWithAdUnitID:ID
+                          request:request
+                completionHandler:^(GADRewardedAd *ad, NSError *error) {
+                  AdMob::AdEvent Ad;
+                  Ad.AdFormat = static_cast<decltype(Ad.AdFormat)>(AdMob::Format::RewardedVideo);
+                  using event = AdMob::RewardedVideoEvent;
+                  if (error) {
+                      logDebug("Rewarded failed load");
+                      //NSLog(@"Rewarded ad failed to load with error: %@", [error localizedDescription]);
+                      Ad.EventType = static_cast<decltype(Ad.EventType)>(event::Failed);
+                  }
+                  else {
+                      logDebug("Rewarded success load");
+                      self.rewardedAd = ad;
+                      self.rewardedAd.fullScreenContentDelegate = self;
+                      Ad.EventType = static_cast<decltype(Ad.EventType)>(event::Loaded);
+                  }
+                  sendCallback(Ad);
+                }];
+        }
+    }
+}
+
+-(void)Show {
+    if(self.rewardedAd) {
+        UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
+        if([self.rewardedAd canPresentFromRootViewController:controller error:nil]) {
+            [self.rewardedAd presentFromRootViewController:controller
+                                  userDidEarnRewardHandler:^ {
+                logDebug("Rewarded");
+                AdMob::AdEvent Ad;
+                Ad.AdFormat = static_cast<decltype(Ad.AdFormat)>(AdMob::Format::RewardedVideo);
+                Ad.EventType = static_cast<decltype(Ad.EventType)>(AdMob::RewardedVideoEvent::Rewarded);
+                sendCallback(Ad);
+            }];
+        }
+    }
+}
+
+/// Tells the delegate that the ad failed to present full screen content.
+- (void)ad:(nonnull id<GADFullScreenPresentingAd>)ad
+didFailToPresentFullScreenContentWithError:(nonnull NSError *)error {
+    logDebug("Ad did fail to present full screen content.");
+    AdMob::AdEvent Ad;
+    Ad.AdFormat = static_cast<decltype(Ad.AdFormat)>(AdMob::Format::RewardedVideo);
+    Ad.EventType = static_cast<decltype(Ad.EventType)>(AdMob::RewardedVideoEvent::Failed);
+    sendCallback(Ad);
+}
+
+/// Tells the delegate that the ad presented full screen content.
+- (void)adDidPresentFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
+    logDebug("Ad did present full screen content.");
+}
+
+/// Tells the delegate that the ad dismissed full screen content.
+- (void)adDidDismissFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
+    logDebug("Ad did dismiss full screen content.");
+    AdMob::AdEvent Ad;
+    Ad.AdFormat = static_cast<decltype(Ad.AdFormat)>(AdMob::Format::RewardedVideo);
+    Ad.EventType = static_cast<decltype(Ad.EventType)>(AdMob::RewardedVideoEvent::Closed);
+    sendCallback(Ad);
+}
+
+- (void)adDidRecordImpression:(nonnull id<GADFullScreenPresentingAd>)ad {
+    logDebug("adDidRecordImpression");
+}
+@end
+
+//
+// INTERFACE METHODS
+//
+bool AdMobiOS::Init(const char* AdMobAppID, int Formats) {
+    [[GADMobileAds sharedInstance] startWithCompletionHandler:nil];
     return true;
 }
 
-bool AdMobiOS::InterstitialSetUnitId(const char* UnitId)
-{
-    [[iAdMob defaultAdMob] InterstitialSetUnitID:UnitId];
+bool AdMobiOS::InterstitialSetUnitId(const char* UnitId) {
+    [[iInterstitial defaultInterstitial] SetUintId: UnitId];
     return true;
 };
 
-bool AdMobiOS::InterstitialLoad()
-{
-    [[iAdMob defaultAdMob] InterstitialLoad];
+bool AdMobiOS::InterstitialLoad() {
+    [[iInterstitial defaultInterstitial] Load];
     return true;
 };
 
-bool AdMobiOS::InterstitialShow()
-{
-    [[iAdMob defaultAdMob] InterstitialShow];
+bool AdMobiOS::InterstitialShow() {
+    [[iInterstitial defaultInterstitial] Show];
     return true;
 };
 
-void AdMobiOS::SetAdEventCallback (AdMob::AdEventCallback * Callback)
-{
-    [[iAdMob defaultAdMob] SetEventCallback: Callback];
+void AdMobiOS::SetAdEventCallback (AdMob::AdEventCallback * Callback) {
+    admobCallback = Callback;
 };
 
-bool AdMobiOS::RewardedVideoSetUnitId(const char* UnitId)
-{
-    [[iAdMob defaultAdMob] RewardedVideoSetUnitID:UnitId];
+bool AdMobiOS::RewardedVideoSetUnitId(const char* UnitId) {
+    [[iRewardedVideo defaultRewardedVideo] SetUintId: UnitId];
     return true;
 };
 
-bool AdMobiOS::RewardedVideoLoad()
-{
-    [[iAdMob defaultAdMob] RewardedVideoLoad];
+bool AdMobiOS::RewardedVideoLoad() {
+    [[iRewardedVideo defaultRewardedVideo] Load];
     return true;
 };
 
-bool AdMobiOS::RewardedVideoShow()
-{
-    [[iAdMob defaultAdMob] RewardedVideoShow];
+bool AdMobiOS::RewardedVideoShow() {
+    [[iRewardedVideo defaultRewardedVideo] Show];
     return true;
 };
