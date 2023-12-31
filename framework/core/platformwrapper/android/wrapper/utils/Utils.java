25d8cc72dccc82d0ad965443290c2df5515fbc86
Amir <amkad87@gmail.com>
2023-12-31 14:41:15 +0400

    fixes

--- framework/core/platformwrapper/android/wrapper/utils/Utils.java
+++ framework/core/platformwrapper/android/wrapper/utils/Utils.java
@@ -98,63 +98,11 @@ public class Utils {
         return _context.getPackageName();
     }
 
-    public static void showMessageBox(int Code, String Title, String Message, String Button1, String Button2, String Button3, long TimeOut)
-    {
-        class OneShotTask implements Runnable {
-            private final int    msgCode;
-            private final String msgTitle;
-            private final String msgMessage;
-            private final String msgButton1;
-            private final String msgButton2;
-            private final String msgButton3;
-            private final long   msgTimeOut;
-
-            private AlertDialog mAlertDialog;
-
-            class TimeOutRunnable implements Runnable {
-                public void run() {
-                    try {
-                        Thread.sleep(msgTimeOut);
-                        if(mAlertDialog != null)
-                            if(mAlertDialog.isShowing())
-                                mAlertDialog.dismiss();
-                    } catch (InterruptedException e) {
-                        Log.e(TAG, "InterruptedException from a ping check thread!");
-                    }
-                }
-            }
-
-            // https://stackoverflow.com/questions/5853167/runnable-with-a-parameter
-            OneShotTask(int Code, String Title, String Message, String Button1, String Button2, String Button3, long TimeOut)
+    public static void showMessageBox(final int msgCode, final String msgTitle, final String msgMessage, final String msgButton1, final String msgButton2, final String msgButton3, final long msgTimeOut) {
+        try {
+            _context.runOnUiThread(() ->
             {
-                msgCode    = Code;
-                msgTitle   = Title;
-                msgMessage = Message;
-                msgButton1 = Button1;
-                msgButton2 = Button2;
-                msgButton3 = Button3;
-                msgTimeOut = TimeOut;
-            }
-            public void run() {
-                try
-                {
-                    /* пример с картинкой
-                    ImageView image = new ImageView(_context);
-                    image.setImageResource(R.drawable.icon);
-
-                    AlertDialog.Builder builder =
-                            new AlertDialog.Builder(_context, android.R.style.Theme_Material_Dialog_Alert).
-                            setMessage("Message above the image").
-                            setView(image).
-                            setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                }
-                            });
-                    builder.create().show();
-                    * */
-
+                try {
                     AlertDialog.Builder builder;
                     //android.os.Build.VERSION.SDK_INT
                     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
@@ -169,10 +117,9 @@ public class Utils {
                                 MessageBoxCallback(msgCode, 1);
                             });
 
-                    //builder.setIcon(android.R.drawable.ic_dialog_alert);
                     builder.setIconAttribute(android.R.attr.alertDialogIcon);
 
-                    if(msgButton2 != null && !msgButton2.isEmpty()) {
+                    if (msgButton2 != null && !msgButton2.isEmpty()) {
                         if (msgButton3 != null && !msgButton3.isEmpty()) {
                             builder.setNeutralButton(msgButton2, (dialog, which) -> {
                                 Log.v(TAG, "NeutralButton click");
@@ -196,34 +143,33 @@ public class Utils {
                         MessageBoxCallback(msgCode, 0);
                     });
 
-                    /*
-                    if (android.os.Build.VERSION.SDK_INT >= 17)
-                        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                            @Override
-                            public void onDismiss(DialogInterface dialog) {
-                                Log.v(TAG, "setOnCancelListener: onDismiss");
-                                MessageBoxCallback(msgCode, 550);
-                            }
-                        });
-                    */
-                    mAlertDialog = builder.create();
+                    final AlertDialog mAlertDialog = builder.create();
                     mAlertDialog.setIcon(0); // убираем иконку
                     mAlertDialog.show();
-                    if(msgTimeOut > 0)
-                    {
-                        Thread mTimeOutRunnable = new Thread(new TimeOutRunnable());
-                        mTimeOutRunnable.start();
+
+                    if (msgTimeOut > 0) {
+                        Thread mTimeOutThread = new Thread(() -> {
+                            try {
+                                Thread.sleep(msgTimeOut);
+                                if (mAlertDialog != null)
+                                    if (mAlertDialog.isShowing()) {
+                                        mAlertDialog.dismiss();
+                                    }
+                            } catch (Exception e) {
+                                Log.e(TAG, e.getMessage());
+                            }
+                        });
+                        mTimeOutThread.start();
                     }
                 }
-                catch(Exception e)
-                {
+                catch(Exception e) {
                     Log.e(TAG, e.getMessage());
                 }
-            }
+            });
         }
-        Log.v(TAG, "before runOnUiThread");
-        _context.runOnUiThread(new OneShotTask(Code, Title, Message, Button1, Button2, Button3, TimeOut));
-        Log.v(TAG, "after runOnUiThread");
+        catch(Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
     }
 
     public static void shareText(String Title, String Message)
@@ -302,43 +248,20 @@ public class Utils {
     }
     */
 
-    public static void showToast(String Msg, int Duration, int Gravity, int xOffset, int yOffset){
-        try
-        {
-            Log.v(TAG, "showToast started");
-            class OneShotTask implements Runnable {
-                private final String toastMsg;
-                private final int toastDuration;
-                private final int toastGravity;
-                private final int toastxOffset;
-                private final int toastyOffset;
-
-                OneShotTask(String Msg, int Duration, int Gravity, int xOffset, int yOffset) {
-                    toastMsg = Msg;
-                    toastDuration = Duration;
-                    toastGravity = Gravity;
-                    toastxOffset = xOffset;
-                    toastyOffset = yOffset;
-                }
-
-                public void run() {
-                    try
-                    {
-                        Toast toast = Toast.makeText(_context, toastMsg, toastDuration);
-                        if (toastGravity >= 0) {
-                            Log.v(TAG, "showToast set toastGravity");
-                            toast.setGravity(toastGravity, toastxOffset, toastyOffset);
-                        }
-                        toast.show();
-                        Log.v(TAG, "showToast after show");
+    public static void showToast(final String Msg, final int Duration, final int Gravity, final int xOffset, final int yOffset){
+        try {
+            _context.runOnUiThread(() -> {
+                try {
+                    Toast toast = Toast.makeText(_context, Msg, Duration);
+                    if (Gravity >= 0) {
+                        toast.setGravity(Gravity, xOffset, yOffset);
                     }
-                    catch(Exception e)
-                    {
-                        Log.e(TAG, e.getMessage());
-                    }
+                    toast.show();
                 }
-            }
-            _context.runOnUiThread(new OneShotTask(Msg, Duration, Gravity, xOffset, yOffset));
+                catch(Exception e) {
+                    Log.e(TAG, e.getMessage());
+                }
+            });
         }
         catch(Exception e)
         {
