df3c22ff22844cce2494a62b0efb9e40aa8ab88a
Amir <amkad87@gmail.com>
2024-03-29 15:17:52 +0400

    optimizing ios build scripts

--- libraries/SDL/ios/lipo_create.sh
+++ libraries/SDL/ios/lipo_create.sh
@@ -3,5 +3,4 @@
 #lipo -create ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2.a ../SDL/Xcode-iOS/SDL/build/Release-iphonesimulator/libSDL2.a -output libSDL2.a
 #cp ../SDL/Xcode-iOS/SDL/build/Release-iphoneos/libSDL2.a libSDL2.a
 
-xcodebuild -project ../SDL/Xcode/SDL/SDL.xcodeproj -target "Static Library-iOS" -configuration Release -sdk iphoneos
-mv -f ../SDL/Xcode/SDL/build/Release-iphoneos/libSDL2.a libSDL2.a
+xcodebuild -project ../SDL/Xcode/SDL/SDL.xcodeproj -target "Static Library-iOS" -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR=$(pwd)
--- libraries/SDL_image/ios/lipo_create.sh
+++ libraries/SDL_image/ios/lipo_create.sh
@@ -1,4 +1,3 @@
-xcodebuild -project ../SDL_Image/Xcode-iOS/SDL_image.xcodeproj -target libSDL_image-iOS -configuration Release -sdk iphoneos HEADER_SEARCH_PATHS=../../../SDL/SDL/include
+xcodebuild -project ../SDL_Image/Xcode-iOS/SDL_image.xcodeproj -target libSDL_image-iOS -configuration Release -sdk iphoneos HEADER_SEARCH_PATHS=../../../SDL/SDL/include  CONFIGURATION_BUILD_DIR=$(pwd)
 
 #lipo -create ../SDL_Image/Xcode-iOS/build/Release-iphoneos/libSDL2_image.a ../SDL_Image/Xcode-iOS/build/Release-iphonesimulator/libSDL2_image.a -output libSDL2_image.a
-mv -f ../SDL_Image/Xcode-iOS/build/Release-iphoneos/libSDL2_image.a libSDL2_image.a
