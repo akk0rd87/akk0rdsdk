4ca9059d133687f577dfbe36fde984ed30cd9ce1
Amir <amkad87@gmail.com>
2024-03-29 14:15:59 +0400

    optimizing ios build scripts

--- libraries/SDL_image/ios/lipo_create.sh
+++ libraries/SDL_image/ios/lipo_create.sh
@@ -1,5 +1,4 @@
-rm libSDL2_image.a
 xcodebuild -project ../SDL_Image/Xcode-iOS/SDL_image.xcodeproj -target libSDL_image-iOS -configuration Release -sdk iphoneos HEADER_SEARCH_PATHS=../../../SDL/SDL/include
 
 #lipo -create ../SDL_Image/Xcode-iOS/build/Release-iphoneos/libSDL2_image.a ../SDL_Image/Xcode-iOS/build/Release-iphonesimulator/libSDL2_image.a -output libSDL2_image.a
-cp ../SDL_Image/Xcode-iOS/build/Release-iphoneos/libSDL2_image.a libSDL2_image.a
+mv -f ../SDL_Image/Xcode-iOS/build/Release-iphoneos/libSDL2_image.a libSDL2_image.a
