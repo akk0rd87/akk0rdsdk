18a501be4376cae81a1f59f24fdd667d56354e1e
Amir <amkad87@gmail.com>
2024-06-02 17:56:43 +0400

    fix gcc mergeDatasets test failure

--- framework/datasetmerger.h
+++ framework/datasetmerger.h
@@ -7,7 +7,7 @@ namespace AkkordSDK {
     namespace Utils {
         template <typename T, typename U>
         auto mergeDatasets(T it1Begin, T it1End, U it2Begin, U it2End) {
-            std::unordered_set<std::remove_const<std::remove_reference<decltype(*std::declval<T>())>::type>::type> result;
+            std::unordered_set<typename std::remove_const<typename std::remove_reference<decltype(*std::declval<T>())>::type>::type> result;
 
             for (; it1Begin != it1End; ++it1Begin) {
                 result.emplace(*it1Begin);
