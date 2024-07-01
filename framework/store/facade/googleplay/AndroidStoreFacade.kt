414c75fb30443ecb72624f44703c44bdcb2bb5bb
Amir <amkad87@gmail.com>
2024-07-01 20:56:19 +0400

    do not use startUpdateFlowForResult deprecated overload

--- framework/store/facade/googleplay/AndroidStoreFacade.kt
+++ framework/store/facade/googleplay/AndroidStoreFacade.kt
@@ -6,6 +6,7 @@ import com.google.android.gms.tasks.Task
 import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
 import com.google.android.play.core.install.InstallState
 import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.AppUpdateType
@@ -19,14 +20,10 @@ class AndroidStoreFacade {
     companion object {
         private val mAppUpdateManager : AppUpdateManager by lazy { AppUpdateManagerFactory.create(org.akkord.lib.Utils.GetContext()) }
 
-        val installStateUpdatedListener = InstallStateUpdatedListener { state: InstallState ->
-            if (state.installStatus() == InstallStatus.DOWNLOADED) {
-                //CHECK THIS if AppUpdateType.FLEXIBLE, otherwise you can skip
-                popupSnackbarForCompleteUpdate()
-            } else if (state.installStatus() == InstallStatus.INSTALLED) {
-                unRegisterUpdateListener()
-            } else {
-                Log.i(org.akkord.lib.Utils.TAG, "InstallStateUpdatedListener: state: ${state.installStatus()}")
+        private val installStateUpdatedListener = InstallStateUpdatedListener { state: InstallState ->
+            when(state.installStatus()) {
+                InstallStatus.DOWNLOADED -> popupSnackbarForCompleteUpdate()
+                InstallStatus.INSTALLED -> unRegisterUpdateListener()
             }
         }
 
@@ -64,8 +61,8 @@ class AndroidStoreFacade {
                         try {
                             mAppUpdateManager.startUpdateFlowForResult(
                                 appUpdateInfo,
-                                AppUpdateType.FLEXIBLE,  /*AppUpdateType.IMMEDIATE*/
                                 org.akkord.lib.Utils.GetContext(),  /*RC_APP_UPDATE*/
+                                AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build(),
                                 100500
                             )
                         } catch (e: java.lang.Exception) {
