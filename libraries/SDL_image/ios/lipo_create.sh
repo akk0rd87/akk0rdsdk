c865c39b2c03199b0044dd393cbc84ecd82cbec9
Amir <amkad87@gmail.com>
2025-06-17 13:29:52 -0700

    change SDL_image/ios/lipo_create.sh

--- libraries/SDL_image/ios/lipo_create.sh
+++ libraries/SDL_image/ios/lipo_create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm *.a
-xcodebuild -project ../SDL_Image/Xcode-iOS/SDL_image.xcodeproj -target libSDL_image-iOS -configuration Release -sdk iphoneos HEADER_SEARCH_PATHS=../../../SDL/SDL/include CONFIGURATION_BUILD_DIR=$(pwd)
+xcodebuild -project ../SDL_Image/Xcode/SDL_image.xcodeproj -target "Static Library" -configuration Release -sdk iphoneos HEADER_SEARCH_PATHS=../../../SDL/SDL/include CONFIGURATION_BUILD_DIR=$(pwd)
