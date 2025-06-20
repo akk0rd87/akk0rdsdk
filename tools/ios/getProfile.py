2f01efad58837fd020dfabe217b27bf8e3a23a6f
Amir <amkad87@gmail.com>
2025-06-20 21:37:07 +0400

    tools/ios/getProfile.py

--- tools/ios/getProfile.py
+++ tools/ios/getProfile.py
@@ -32,7 +32,7 @@ def getProfile(app_bundle_name):
 
     for item in data["data"]:
         attrs = item['attributes']
-        if(attrs['profileState'] == 'ACTIVE' and attrs['profileType'] == 'IOS_APP_STORE' and attrs['name'] == 'AllApps') :
+        if(attrs['profileState'] == 'ACTIVE' and attrs['profileType'] == 'IOS_APP_STORE') :
             print(f"Found profile for [{app_bundle_name}]: Bundleid = {appBundleId}")
             print(f"profile name : {attrs['name']}")
             print(f"ProfileID = {item['id']}")
