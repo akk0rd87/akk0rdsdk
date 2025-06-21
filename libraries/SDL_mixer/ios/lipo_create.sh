9e392571031d9d86291fcf56bd188ca0bd129957
Amir <amkad87@gmail.com>
2025-06-22 02:16:19 +0400

    libraries/SDL_mixer/ios/lipo_create.sh

--- libraries/SDL_mixer/ios/lipo_create.sh
+++ libraries/SDL_mixer/ios/lipo_create.sh
@@ -1,5 +1,6 @@
 rm libSDL2_mixer.a
-xcodebuild -project ../SDL_mixer/Xcode-iOS/SDL_mixer.xcodeproj -target libSDL_mixer-iOS -configuration Release -sdk iphoneos
-#xcodebuild -project ../SDL_mixer/Xcode-iOS/SDL_mixer.xcodeproj -target libSDL_mixer-iOS -configuration Release -sdk iphonesimulator
+xcodebuild -project ../SDL_mixer/Xcode/SDL_mixer.xcodeproj -target "Static Library" -configuration Release -sdk iphoneos
+# xcodebuild -project ../SDL_mixer/Xcode/SDL_mixer.xcodeproj -target "Static Library" -configuration Release -sdk iphonesimulator
+
 #lipo -create ../SDL_mixer/Xcode-iOS/build/Release-iphoneos/libSDL2_mixer.a ../SDL_mixer/Xcode-iOS/build/Release-iphonesimulator/libSDL2_mixer.a -output libSDL2_mixer.a
-cp ../SDL_mixer/Xcode-iOS/build/Release-iphoneos/libSDL2_mixer.a libSDL2_mixer.a
+cp ../SDL_mixer/Xcode/build/Release-iphoneos/libSDL2_mixer.a libSDL2_mixer.a
