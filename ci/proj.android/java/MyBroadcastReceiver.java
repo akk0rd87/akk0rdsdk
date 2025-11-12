381065bf3b42b2a33870a4f661bc1316f9269305
Amir <amkad87@gmail.com>
2025-11-12 11:57:32 +0400

    org.gradle.jvmargs=-Xmx4096m

--- ci/proj.android/java/MyBroadcastReceiver.java
+++ ci/proj.android/java/MyBroadcastReceiver.java
@@ -1,24 +1,12 @@
 package org.popapp.citest;
 
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
-import static android.content.Intent.EXTRA_CHOSEN_COMPONENT;
-import static org.popapp.citest.MainActivity.TAG;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        try {
-            //ComponentName clickedComponent = intent.getParcelableExtra(EXTRA_CHOSEN_COMPONENT);
-            //Log.v(TAG, "MyBroadcastReceiver: onReceive: " + clickedComponent.toShortString());
-            //org.popapp.citest.MainActivity.logImageSendEvent(clickedComponent.toShortString());
-        }
-        catch(Exception e) {
-            //Log.e(TAG, e.getMessage());
-        }
     }
 }
