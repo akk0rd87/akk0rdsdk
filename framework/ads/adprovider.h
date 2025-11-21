59b89f1497930dd544da5653f9e1004df7806972
Amir <amkad87@gmail.com>
2025-11-21 14:36:32 +0400

    decrease interstitialLoadDelaySeconds and rewardedVideoLoadDelaySeconds

--- framework/ads/adprovider.h
+++ framework/ads/adprovider.h
@@ -11,7 +11,7 @@ namespace ads {
 
     class Provider {
     public:
-        Provider(std::weak_ptr<ProviderCallback> callback, timeMS interstitialLoadDelaySeconds = 5, timeMS rewardedVideoLoadDelaySeconds = 5) :
+        Provider(std::weak_ptr<ProviderCallback> callback, timeMS interstitialLoadDelaySeconds = 1, timeMS rewardedVideoLoadDelaySeconds = 1) :
             callback(callback),
             lastIntLoadTime(0),
             lastRewLoadTime(0),
