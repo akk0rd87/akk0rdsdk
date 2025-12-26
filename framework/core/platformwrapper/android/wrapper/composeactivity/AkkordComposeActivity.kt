1d5b07e70b8dd53895c17c8e5df44c61c6f3a45d
Amir <amkad87@gmail.com>
2025-12-26 10:44:16 +0400

    Выносим org.akkord.lib.Utils.checkUpdate() из AkkordComposeActivity::onCreate чтобы не открывать никаких окон до инициализации compose view

--- framework/core/platformwrapper/android/wrapper/composeactivity/AkkordComposeActivity.kt
+++ framework/core/platformwrapper/android/wrapper/composeactivity/AkkordComposeActivity.kt
@@ -5,16 +5,25 @@ import androidx.activity.ComponentActivity
 import com.getkeepsafe.relinker.ReLinker
 
 open class AkkordComposeActivity : ComponentActivity() {
+    private var isFirstResume = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         org.akkord.lib.Utils.Init(this)
-        org.akkord.lib.Utils.checkUpdate()
         loadNativeLibs()
     }
 
     override fun onStop() {
         super.onStop()
         org.akkord.lib.Utils.onActivityStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(isFirstResume) {
+            isFirstResume = false
+            org.akkord.lib.Utils.checkUpdate()
+        }
     }
 
     private fun loadNativeLib(libName : String) {
